d890bbbfadd2f5a30e284f610e968ad88bd342d7
Damian <damian@code4sa.org>
2017-08-14 15:36:19 +0200

    Fix embed.js file path

--- js/embed.js
+++ js/embed.js
@@ -6,4 +6,4 @@ if (document.location.hostname == "localhost") {
 document.write('<div id="min-wage-embed-parent"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + '/bower_components/pym.js/dist/pym.v1.min.js" crossorigin="anonymous"></script>');
 
-document.write("<script>var pymParent = new pym.Parent('min-wage-embed-parent', '" + baseurl + "index.html', {});</script>");
+document.write("<script>var pymParent = new pym.Parent('min-wage-embed-parent', '" + baseurl + "/index.html', {});</script>");

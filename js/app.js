6681752441dba2e45c6b28f7ff2f45d1047d7405
Damian <damian@code4sa.org>
2017-08-15 14:30:28 +0200

    Calculate balance

--- js/app.js
+++ js/app.js
@@ -12,11 +12,12 @@ $(window).on('load', function() {
     var hh_income = parseInt($('#hh-income').val()),
         hh_members = parseInt($('#hh-members').val()),
         hh_children = parseInt($('#hh-children').val()),
-        pocket = 0,
+        balance = 0,
+        total_exp = 0,
         exp_obj = {};
 
 
-    function populate_exp_obj () {
+    function fill_exp_obj () {
       $('#expense-categories').children().each( function () {
         var key = $(this).attr('id'),
             perc = parseFloat($(this).find('.perc').text());
@@ -27,15 +28,41 @@ $(window).on('load', function() {
       });
     }
 
-    function fill_initial_amounts() {
+    function fill_initial_epxense_amounts() {
       $('#expense-categories').children().each( function () {
         var key = $(this).attr('id');
-        $(this).find('.amount').text(exp_obj[key]['amount']);
+        $(this).find('.amount').text(round(exp_obj[key]['amount']));
       });
     }
 
-    populate_exp_obj()
-    fill_initial_amounts();
+    function calc_total_expense() {
+      var total = 0;
+      for(var key in exp_obj) {
+        total += exp_obj[key]['amount'];
+      }
+      total_exp = total;
+    }
+
+    function fill_balance() {
+      var balance = hh_income - total_exp;
+      $('#balance').find('.amount').text(round(balance));
+      $('#balance').removeClass("negative positive").addClass(balance < 0 ? 'negative' : 'positive');
+    }
+
+    function round(n) {
+      return Math.round(n);
+    }
+
+    fill_exp_obj()
+    fill_initial_epxense_amounts();
+    calc_total_expense()
+    fill_balance()
+
+    $('#hh-income').keyup(function() {
+      hh_income = $(this).val();
+      calc_total_expense();
+      fill_balance();
+    });
 
 
   });
